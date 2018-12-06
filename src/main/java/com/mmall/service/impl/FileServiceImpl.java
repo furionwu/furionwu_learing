5812d11a8d005caead268c08015eea6ad97c3703
furionwu <fly_wushuai@foxmail.com>
2018-12-07 00:28:13 +0800

    add upload

--- src/main/java/com/mmall/service/impl/FileServiceImpl.java
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -1,8 +1,11 @@
 package com.mmall.service.impl;
 
+import com.google.common.collect.Lists;
 import com.mmall.service.IFileService;
+import com.mmall.util.FTPUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -10,6 +13,7 @@ import java.io.IOException;
 import java.util.UUID;
 
 
+@Service("iFileService")
 public class FileServiceImpl implements IFileService {
 
     private Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
@@ -28,6 +32,8 @@ public class FileServiceImpl implements IFileService {
         File targetFile = new File(path,uploadFileName);
         try {
             file.transferTo(targetFile);
+            FTPUtil.uploadFile(Lists.<File>newArrayList(targetFile));
+            targetFile.delete();
         } catch (IOException e) {
           logger.error("上传文件异常",e);
           return null;
